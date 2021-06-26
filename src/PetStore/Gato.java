74b90778772b3ba92eee5b500e5e41badc077333
Kaio Cândido Santiago <62676087+Kaio1394@users.noreply.github.com>
2021-06-26 11:06:46 -0300

    Add files via upload

--- src/PetStore/Gato.java
+++ src/PetStore/Gato.java
@@ -2,13 +2,16 @@ package PetStore;
 
 public class Gato extends Mamifero {
 
+	public Gato(String nome, int idade, String dono, String cuidadosProprios, 
+			String alimentacao) {
+		super(nome, idade, dono, cuidadosProprios, alimentacao);
+		this.especie = "Gato";
+	}
+
 	private static final long serialVersionUID = 1L;
 
 	public String soar() {
 		return "Faz Miados";
 	}
-	public Gato(String nome, int idade, String dono) {
-		super(nome, idade, dono);
-		this.especie = "Gato";
-	}
+	
 }
